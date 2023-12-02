a37f11d6e2e40eb075f6df89c410db333d1f6528
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2023-12-02 16:21:26 +0100

    day 2

---
+++ cmd/day021/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/alecthomas/participle/v2"
+	"github.com/mgradowski/aoc2023/pkg/day02"
+)
+
+var defaultMaxColors = map[string]int{
+	"red":   12,
+	"green": 13,
+	"blue":  14,
+}
+
+func main() {
+	parser := participle.MustBuild[day02.Game]()
+	scanner := bufio.NewScanner(os.Stdin)
+	result := 0
+
+	for scanner.Scan() {
+		game, err := parser.ParseString("stdin", scanner.Text())
+		if err != nil {
+			log.Panic(err)
+		}
+
+		if !game.IsLegal(defaultMaxColors) {
+			continue
+		}
+		result += game.Id
+	}
+
+	fmt.Println(result)
+}
---
+++ cmd/day022/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/alecthomas/participle/v2"
+	"github.com/mgradowski/aoc2023/pkg/day02"
+)
+
+func main() {
+	parser := participle.MustBuild[day02.Game]()
+	scanner := bufio.NewScanner(os.Stdin)
+	result := 0
+
+	for scanner.Scan() {
+		game, err := parser.ParseString("stdin", scanner.Text())
+		if err != nil {
+			log.Panic(err)
+		}
+		result += game.Power()
+	}
+
+	fmt.Println(result)
+}
