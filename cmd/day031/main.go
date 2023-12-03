7c539cc76af4680b5965cc38f7db7cede4ba2374
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2023-12-03 20:22:48 +0100

    day 3

---
+++ cmd/day031/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/mgradowski/aoc2023/pkg/day03"
+)
+
+func main() {
+	parser := day03.NewParser()
+	stdin, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	schematic, err := parser.ParseString("stdin", string(stdin))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(schematic.Part1())
+}
